520bda6639012369201c8c4d27dfe03de54acc08
Saurabh Sharma <113715335+sasha643@users.noreply.github.com>
2023-06-09 23:00:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import smtplib
 import os
 import string
 from werkzeug.utils import secure_filename
+import re
 
 
 srt=''
@@ -64,6 +65,27 @@ def jawab():
                     o = " ".join(line.strip() for line in query)
                     new = o.replace(" ", "")
                     new_string = new.translate(str.maketrans('', '', string.punctuation))
+                    emoji_pattern = re.compile("["
+                               u"\U0001F600-\U0001F64F"  
+                               u"\U0001F300-\U0001F5FF"  
+                               u"\U0001F680-\U0001F6FF" 
+                               u"\U0001F1E0-\U0001F1FF"  
+                               u"\U00002500-\U00002BEF"  
+                               u"\U00002702-\U000027B0"
+                               u"\U00002702-\U000027B0"
+                               u"\U000024C2-\U0001F251"
+                               u"\U0001f926-\U0001f937"
+                               u"\U00010000-\U0010ffff"
+                               u"\u2640-\u2642"
+                               u"\u2600-\u2B55"
+                               u"\u200d"
+                               u"\u23cf"
+                               u"\u23e9"
+                               u"\u231a"
+                               u"\ufe0f" 
+                               u"\u3030"
+                               "]+", flags=re.UNICODE)
+                    new_string = emoji_pattern.sub(r'', new_string)
                     if f.filename.split(".")[-1] == 'jpg' or f.filename.split(".")[-1] == 'jpeg' or f.filename.split(".")[-1] == 'png':
                         resp=supabase.storage.from_("data").upload('name-{}'.format(new_string),upload_path,{"content-type":"image/jpeg"})
                     if f.filename.split(".")[-1] == 'pdf':
